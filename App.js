946585ed5d4a504c54a73445ba06063f92d88339
Karan Gupta <d22124440@mytudublin.ie>
2023-07-04 15:03:44 +0100

    wip

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Tabs from "./src/components/Tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color={"chocolate"} />
+      </View>
+    );
+  }
   return (
     <NavigationContainer>
       <Tabs />
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+});
 
 export default App;
