82c4b90e8576e06fb8f8e78cdd5e8de1b4947c54
Karan Gupta <d22124440@mytudublin.ie>
2023-07-06 00:53:53 +0100

    weather data fetched

--- App.js
+++ App.js
@@ -1,10 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Tabs from "./src/components/Tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import * as Location from "expo-location";
+import { WEATHER_API_KEY } from "@env";
+
+//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  // const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
+  const [weather, setWeather] = useState([]);
+  const [lat, setLat] = useState([]);
+  const [lon, setLon] = useState([]);
+
+  const fetchWeatherData = async () => {
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+      );
+      const data = await res.json();
+      setWeather(data);
+    } catch (e) {
+      setError("Couldn't load weather data");
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setError("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      // setLocation(location);
+      setLat(location.coords.latitude);
+      setLon(location.coords.longitude);
+      await fetchWeatherData();
+    })();
+  }, [lat, lon]);
+
+  // console.log(WEATHER_API_KEY);
+
+  if (weather) {
+    console.log(weather);
+  }
+
   if (loading) {
     return (
       <View style={styles.container}>
@@ -12,6 +57,7 @@ const App = () => {
       </View>
     );
   }
+
   return (
     <NavigationContainer>
       <Tabs />
